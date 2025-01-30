676448d3ca0c54d6d3a09f3be7bcaa7017fb2a74
Rohit <dv6510@gmail.com>
2025-01-30 11:33:48 +0530

    done

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const { resolve } = require('path');
-
 const app = express();
-const port = 3010;
+const port = process.env.PORT || 3000;
+
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('Connected to MongoDB Atlas');
+  } catch (error) {
+    console.error('Error connecting to MongoDB Atlas:', error);
+  }
+}
+
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Example app listening at http://localhost:${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB Atlas:', error);
+  });
 
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
 });
